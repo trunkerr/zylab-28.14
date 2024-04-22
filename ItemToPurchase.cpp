25deaa2160977960b509c3f562dac88eafc70d94
trunkerr <167352399+trunkerr@users.noreply.github.com>
2024-04-22 13:46:29 -0500

    Update ItemToPurchase.cpp

--- ItemToPurchase.cpp
+++ ItemToPurchase.cpp
@@ -1,1 +1,12 @@
-/* Type your code here */
+#include <iostream>
+#include <string>
+using namespace std;
+
+class ItemtoPurchasse {
+private:
+  string itemName;
+  string itemDescription;
+  int itemPrice;
+  int itemQuantity;
+
+public:
