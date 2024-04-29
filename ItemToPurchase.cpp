4688f700e4372eb8931246e8e9984a0f647ba0e6
trunkerr <167352399+trunkerr@users.noreply.github.com>
2024-04-28 23:46:03 -0500

    update

--- ItemToPurchase.cpp
+++ ItemToPurchase.cpp
@@ -4,6 +4,18 @@
 using namespace std;
 
 //Default Constructor
-ItemtoPurchase::ItemToPurchase() : itemName("none"), itemDescription("none"), itemDescription("none"), itemPrice(0), itemQuantity(0) {}
+ItemtoPurchase::ItemToPurchase() : itemName("none"), itemDescription("none"), itemDescription("none"), 
+    itemPrice(0), itemQuantity(0) {}
 
-ItemtoPurchase::ItemToPurchase(string  name, string description, int price, int quantity) : itemName(name)
+ItemtoPurchase::ItemToPurchase(string  name, string description, int price, int quantity) : itemName(name), 
+    itemDescription(description), itemPrice(price), itemQuantity(quantity) {}
+
+// Sets item name
+void ItemToPurchase::SetName(string name) {
+    itemName = name;
+}
+
+// Returns item name
+string ItemtoPurchase::GetName() const {
+    return itemName;
+}
